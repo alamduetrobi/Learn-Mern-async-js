01a3d9a783422417d28a638f4d096ff08fc0c37b
alamduetrobi <alamrobiduet074001@gmail.com>
2022-12-15 23:11:55 +0600

    1 file modified

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 import './style.css';
 
 // Write Javascript code!
+
+
+console.log("Hello World")
+
+
+
+
+
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Rabiul Backend Developer</h1>`;
